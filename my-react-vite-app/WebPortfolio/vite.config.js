17cadfa48e16a8b0fdc36b0703da8f53a8f95dfb
Evelyn <etorres.et905@gmail.com>
2023-12-22 18:41:33 -0500

    deleted folder

--- my-react-vite-app/WebPortfolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default {
-  build: {
-    outDir: 'dist', // Change this to '.' or 'public' or any desired folder
-  },
-};
